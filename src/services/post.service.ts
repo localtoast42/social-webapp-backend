fcfd65c84d36d529401edb1fbdfff7c040964b36
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-08-05 15:31:57 -0400

    Implement Post service with Prisma ORM

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -1,53 +1,48 @@
-import {
-  FilterQuery,
-  ProjectionType,
-  QueryOptions,
-  UpdateQuery,
-} from "mongoose";
-import PostModel, { Post, PostCreate } from "../models/post.model";
-import { User } from "../models/user.model";
+import { Prisma } from "@prisma/client";
+import prisma from "../utils/client";
 import logger from "../utils/logger";
 
-export async function createPost(input: PostCreate) {
+export async function createPost(input: Prisma.PostCreateInput) {
   try {
-    const post = await PostModel.create(input);
-    return post.toJSON();
+    return prisma.post.create({ data: input });
   } catch (e: any) {
     logger.error(e);
     throw new Error(e);
   }
 }
 
-export async function findPost(query: FilterQuery<Post>) {
-  const result = PostModel.findOne(query)
-    .populate<{ author: User }>("author", "-password")
-    .lean({ virtuals: true });
-  return result;
+export async function findPost(query: Prisma.PostFindUniqueArgs) {
+  try {
+    return prisma.post.findUnique(query);
+  } catch (e: any) {
+    logger.error(e);
+    throw new Error(e);
+  }
 }
 
-export async function findManyPosts(
-  query: FilterQuery<Post>,
-  projection?: ProjectionType<Post>,
-  options?: QueryOptions
-) {
-  const result = PostModel.find(query, projection, options)
-    .populate<{ author: User }>("author", "-password")
-    .lean({ virtuals: true });
-  return result;
+export async function findManyPosts(query: Prisma.PostFindManyArgs) {
+  try {
+    return prisma.post.findMany(query);
+  } catch (e: any) {
+    logger.error(e);
+    throw new Error(e);
+  }
 }
 
-export async function findPostsByUser(query: FilterQuery<Post>) {
-  return PostModel.findOne(query).lean();
+export async function findAndUpdatePost(query: Prisma.PostUpdateArgs) {
+  try {
+    return prisma.post.update(query);
+  } catch (e: any) {
+    logger.error(e);
+    throw new Error(e);
+  }
 }
 
-export async function findAndUpdatePost(
-  query: FilterQuery<Post>,
-  update: UpdateQuery<Post>,
-  options: QueryOptions
-) {
-  return PostModel.findOneAndUpdate(query, update, options);
-}
-
-export async function deletePost(query: FilterQuery<Post>) {
-  return PostModel.deleteOne(query);
+export async function deletePost(query: Prisma.PostDeleteArgs) {
+  try {
+    return prisma.post.delete(query);
+  } catch (e: any) {
+    logger.error(e);
+    throw new Error(e);
+  }
 }
