aba8625b2016bf1184776791f0121aa94983462e
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-08-06 17:24:13 -0400

    Convert user controller to use Prisma ORM fxns

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,12 +1,6 @@
 import { Request, Response } from "express";
 import config from "config";
-import {
-  FilterQuery,
-  ProjectionType,
-  QueryOptions,
-  UpdateQuery,
-} from "mongoose";
-import { omit } from "lodash";
+import { Prisma } from "@prisma/client";
 import logger from "../utils/logger";
 import {
   createUser,
@@ -14,21 +8,21 @@ import {
   findManyUsers,
   findAndUpdateUser,
   deleteUser,
-  FindUserResult,
+  findUserWithFollowing,
+  UserWithAllFollows,
 } from "../services/user.service";
 import {
-  CreateUserInput,
-  DeleteUserInput,
-  FollowUserInput,
-  PopulateUsersInput,
-  ReadUserInput,
-  UpdateUserInput,
+  CreateUserRequest,
+  DeleteUserRequest,
+  FollowUserRequest,
+  PopulateUsersRequest,
+  ReadUserRequest,
+  UpdateUserRequest,
 } from "../schemas/user.schema";
 import { createRandomPost, createRandomUser } from "../utils/populateDatabase";
-import { User } from "../models/user.model";
 
 export async function createUserHandler(
-  req: Request<{}, {}, CreateUserInput["body"]>,
+  req: Request<{}, {}, CreateUserRequest["body"]>,
   res: Response
 ) {
   req.body.isGuest = !config.get<boolean>("allowNewPublicUsers");
@@ -44,81 +38,78 @@ export async function createUserHandler(
 }
 
 export async function getUserHandler(
-  req: Request<ReadUserInput["params"]>,
+  req: Request<ReadUserRequest["params"]>,
   res: Response
 ) {
-  const requestingUser: FindUserResult = res.locals.user;
-  const requestingUserId = requestingUser.id;
+  // const requestingUser = res.locals.user;
+  // const requestingUserId = requestingUser.id;
   const userId = req.params.userId;
 
-  const user = await findUser({ _id: userId });
+  const user = await findUser({
+    where: { id: userId },
+  });
 
   if (!user) {
     return res.sendStatus(404);
   }
 
-  const userObject = omit(user.toJSON(), "password");
+  // user.followedByMe = user.followedBy.includes(requestingUserId);
 
-  userObject.followedByMe = userObject.followers.includes(requestingUserId);
-
-  return res.json(userObject);
+  return res.json(user);
 }
 
 export async function getSelfHandler(req: Request, res: Response) {
-  const user: FindUserResult = res.locals.user;
+  const user: UserWithAllFollows = res.locals.user;
 
-  return res.json(omit(user.toJSON(), "password"));
+  return res.json(user);
 }
 
 export async function getUserListHandler(req: Request, res: Response) {
-  const user: FindUserResult = res.locals.user;
-  const userId = user._id;
+  const user: NonNullable<UserWithAllFollows> = res.locals.user;
+  const userId = user.id;
 
   const queryTerms: object[] = [];
-  queryTerms.push({ _id: { $exists: true } });
 
   if (req.query.q) {
     const queryString = req.query.q as string;
-    let regex = new RegExp(queryString, "i");
-    queryTerms.push({ $or: [{ firstName: regex }, { lastName: regex }] });
+    queryTerms.push({
+      OR: [
+        { firstName: { contains: queryString } },
+        { lastName: { contains: queryString } },
+      ],
+    });
   }
 
-  const query: FilterQuery<User> = {
-    _id: { $ne: userId },
-    isGuest: false,
-    $and: queryTerms,
+  const query: Prisma.UserFindManyArgs = {
+    select: {
+      username: true,
+      firstName: true,
+      lastName: true,
+      city: true,
+      state: true,
+      country: true,
+      imageUrl: true,
+    },
+    where: {
+      AND: [{ id: { not: userId } }, { isGuest: false }, ...queryTerms],
+    },
+    orderBy: { lastName: "asc" },
   };
 
-  const projection: ProjectionType<User> = {
-    username: 1,
-    firstName: 1,
-    lastName: 1,
-    city: 1,
-    state: 1,
-    country: 1,
-    imageUrl: 1,
-    fullName: 1,
-    url: 1,
-  };
-
-  const options: QueryOptions = {
-    sort: { lastName: 1 },
-  };
-
-  const users = await findManyUsers(query, projection, options);
+  const users = await findManyUsers(query);
 
   return res.json({ data: users });
 }
 
 export async function updateUserHandler(
-  req: Request<UpdateUserInput["params"], {}, UpdateUserInput["body"]>,
+  req: Request<UpdateUserRequest["params"], {}, UpdateUserRequest["body"]>,
   res: Response
 ) {
-  const requestingUser: FindUserResult = res.locals.user;
+  const requestingUser: NonNullable<UserWithAllFollows> = res.locals.user;
   const requestingUserId = requestingUser.id;
   const userId = req.params.userId;
 
-  const user = await findUser({ _id: userId });
+  const user = await findUser({ where: { id: userId } });
 
   if (!user) {
     return res.sendStatus(404);
@@ -128,24 +119,27 @@ export async function updateUserHandler(
     return res.sendStatus(403);
   }
 
-  const update: UpdateQuery<User> = req.body;
+  const update = req.body;
 
-  const updatedUser = await findAndUpdateUser({ _id: userId }, update, {
-    new: true,
-  });
+  const query: Prisma.UserUpdateArgs = {
+    where: { id: userId },
+    data: { ...update },
+  };
 
-  return res.json(omit(updatedUser?.toJSON(), "password"));
+  const updatedUser = await findAndUpdateUser(query);
+
+  return res.json(updatedUser);
 }
 
 export async function deleteUserHandler(
-  req: Request<DeleteUserInput["params"]>,
+  req: Request<DeleteUserRequest["params"]>,
   res: Response
 ) {
-  const requestingUser: FindUserResult = res.locals.user;
+  const requestingUser: NonNullable<UserWithAllFollows> = res.locals.user;
   const requestingUserId = requestingUser.id;
   const userId = req.params.userId;
 
-  const user = await findUser({ _id: userId });
+  const user = await findUser({ where: { id: userId } });
 
   if (!user) {
     return res.sendStatus(404);
@@ -155,7 +149,7 @@ export async function deleteUserHandler(
     return res.sendStatus(403);
   }
 
-  const result = await deleteUser({ _id: userId });
+  const result = await deleteUser({ where: { id: userId } });
 
   return res.json({
     ...result,
@@ -165,12 +159,12 @@ export async function deleteUserHandler(
 }
 
 export async function getUserFollowsHandler(
-  req: Request<ReadUserInput["params"]>,
+  req: Request<ReadUserRequest["params"]>,
   res: Response
 ) {
   const userId = req.params.userId;
 
-  const userFollows = await findUser({ _id: userId }, "following");
+  const userFollows = await findUserWithFollowing({ id: userId });
 
   if (userFollows === null) {
     return res.sendStatus(404);
@@ -180,51 +174,34 @@ export async function getUserFollowsHandler(
 }
 
 export async function followUserHandler(
-  req: Request<FollowUserInput["params"], {}, FollowUserInput["body"]>,
+  req: Request<FollowUserRequest["params"], {}, FollowUserRequest["body"]>,
   res: Response
 ) {
-  const follow = JSON.parse(req.body.follow);
-  const requestingUser: FindUserResult = res.locals.user;
+  const requestingUser: NonNullable<UserWithAllFollows> = res.locals.user;
+  const requestingUserId = requestingUser.id;
   const targetUserId = req.params.userId;
+  const follow: boolean = JSON.parse(req.body.follow);
 
-  const targetUser = await findUser({ _id: targetUserId });
+  const targetUser = await findUser({ where: { id: targetUserId } });
 
   if (!targetUser) {
     return res.sendStatus(404);
   }
 
-  requestingUser.following = requestingUser.following.filter((userId) => {
-    return userId.toString() !== targetUser._id.toString();
+  const update = follow
+    ? { connect: { id: targetUserId } }
+    : { disconnect: { id: targetUserId } };
+
+  const updatedUser = await findAndUpdateUser({
+    where: { id: requestingUserId },
+    data: { followedBy: update },
   });
-  targetUser.followers = targetUser.followers.filter((userId) => {
-    return userId.toString() !== requestingUser._id.toString();
-  });
 
-  if (follow) {
-    requestingUser.following.push(targetUser._id);
-    targetUser.followers.push(requestingUser._id);
-  }
-
-  const requestingUserUpdates: UpdateQuery<User> = {
-    following: requestingUser.following,
-  };
-
-  const targetUserUpdates: UpdateQuery<User> = {
-    followers: targetUser.followers,
-  };
-
-  await Promise.all([
-    findAndUpdateUser({ _id: requestingUser.id }, requestingUserUpdates, {
-      new: true,
-    }),
-    findAndUpdateUser({ _id: targetUserId }, targetUserUpdates, { new: true }),
-  ]);
-
-  return res.sendStatus(200);
+  return res.json(updatedUser);
 }
 
 export async function populateUsers(
-  req: Request<{}, {}, PopulateUsersInput["body"]>,
+  req: Request<{}, {}, PopulateUsersRequest["body"]>,
   res: Response
 ) {
   const userCount = req.body.userCount;
@@ -234,7 +211,7 @@ export async function populateUsers(
     let user = await createRandomUser();
 
     for (let j = 0; j < postCount; j++) {
-      createRandomPost(user._id);
+      createRandomPost(user.id);
     }
   }
 
