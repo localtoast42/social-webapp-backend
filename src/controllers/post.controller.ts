55af3a767a99e1862a3cf23e70751129ad892920
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-08-06 22:23:37 -0400

    Convert post controller to use Prisma ORM

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { QueryOptions, UpdateQuery } from "mongoose";
+import { Prisma } from "@prisma/client";
 import {
   CreatePostInput,
   ReadPostInput,
@@ -14,29 +14,26 @@ import {
   findAndUpdatePost,
   deletePost,
   findManyPosts,
+  findFollowedPosts,
 } from "../services/post.service";
-import { deleteManyComments } from "../services/comment.service";
-import { FindUserResult } from "../services/user.service";
-import { Post } from "../models/post.model";
+import { UserWithAllFollows } from "../services/user.service";
 
 export async function createPostHandler(
   req: Request<{}, {}, CreatePostInput["body"]>,
-  res: Response
+  res: Response<{}, { user: UserWithAllFollows }>
 ) {
-  const user: FindUserResult = res.locals.user;
+  const user = res.locals.user;
 
   if (!user) {
     return res.sendStatus(403);
   }
 
   const body = req.body;
-  const postDate = new Date(Date.now());
 
   const post = await createPost({
     ...body,
-    author: user._id,
-    postDate: postDate,
-    isPublicPost: !user.isGuest,
+    isPublic: !user.isGuest,
+    author: { connect: { id: user.id } },
   });
 
   return res.status(201).json(post);
@@ -48,7 +45,7 @@ export async function getPostHandler(
 ) {
   const postId = req.params.postId;
 
-  const post = await findPost({ _id: postId });
+  const post = await findPost({ where: { id: postId } });
 
   if (!post) {
     return res.sendStatus(404);
@@ -57,27 +54,33 @@ export async function getPostHandler(
   return res.json(post);
 }
 
-export async function getRecentPostsHandler(req: Request, res: Response) {
-  const user: FindUserResult = res.locals.user;
-  const userId = user._id;
+export async function getRecentPostsHandler(
+  req: Request,
+  res: Response<{}, { user: UserWithAllFollows }>
+) {
+  const user = res.locals.user;
 
-  const query = {
-    $or: [{ author: userId }, { isPublicPost: true }],
-  };
+  if (!user) {
+    return res.sendStatus(403);
+  }
 
-  const options: QueryOptions = {
-    sort: { postDate: -1 },
+  const query: Prisma.PostFindManyArgs = {
+    where: {
+      parentId: null,
+      OR: [{ authorId: user.id }, { isPublic: true }],
+    },
+    orderBy: { createdAt: "desc" },
   };
 
   if (req.query.limit) {
-    options.limit = parseInt(req.query.limit as string);
+    query.take = parseInt(req.query.limit as string);
   }
 
   if (req.query.skip) {
-    options.skip = parseInt(req.query.skip as string);
+    query.skip = parseInt(req.query.skip as string);
   }
 
-  const posts = await findManyPosts(query, {}, options);
+  const posts = await findManyPosts(query);
 
   if (!posts) {
     return res.sendStatus(404);
@@ -86,29 +89,23 @@ export async function getRecentPostsHandler(req: Request, res: Response) {
   return res.json({ data: posts });
 }
 
-export async function getFollowedPostsHandler(req: Request, res: Response) {
-  const user: FindUserResult = res.locals.user;
-  const userId = user._id;
-  const following = user.following;
+export async function getFollowedPostsHandler(
+  req: Request,
+  res: Response<{}, { user: UserWithAllFollows }>
+) {
+  const user = res.locals.user;
 
-  const query = {
-    author: { $in: [...following, userId] },
-    isPublicPost: true,
-  };
-
-  const options: QueryOptions = {
-    sort: { postDate: -1 },
-  };
-
-  if (req.query.limit) {
-    options.limit = parseInt(req.query.limit as string);
+  if (!user) {
+    return res.sendStatus(403);
   }
 
-  if (req.query.skip) {
-    options.skip = parseInt(req.query.skip as string);
-  }
+  const take = req.query.limit
+    ? parseInt(req.query.limit as string)
+    : undefined;
 
-  const posts = await findManyPosts(query, {}, options);
+  const skip = req.query.skip ? parseInt(req.query.skip as string) : undefined;
+
+  const posts = await findFollowedPosts(user.id, take, skip);
 
   if (!posts) {
     return res.sendStatus(404);
@@ -119,30 +116,28 @@ export async function getFollowedPostsHandler(req: Request, res: Response) {
 
 export async function getPostsByUserHandler(
   req: Request<ReadPostByUserInput["params"]>,
-  res: Response
+  res: Response<{}, { user: UserWithAllFollows }>
 ) {
-  const requestingUser: FindUserResult = res.locals.user;
-  const requestingUserId = requestingUser._id;
+  const requestingUser = res.locals.user;
   const targetUserId = req.params.userId;
 
-  let query = {};
-
-  if (targetUserId === requestingUserId.toString()) {
-    query = {
-      author: targetUserId,
-    };
-  } else {
-    query = {
-      author: targetUserId,
-      isPublicPost: true,
-    };
+  if (!requestingUser) {
+    return res.sendStatus(403);
   }
 
-  const options: QueryOptions = {
-    sort: { postDate: -1 },
+  const query: Prisma.PostFindManyArgs = {
+    where: {
+      parentId: null,
+      authorId: targetUserId,
+      OR: [
+        { isPublic: true },
+        { isPublic: targetUserId !== requestingUser.id },
+      ],
+    },
+    orderBy: { createdAt: "desc" },
   };
 
-  const posts = await findManyPosts(query, {}, options);
+  const posts = await findManyPosts(query);
 
   if (!posts) {
     return res.sendStatus(404);
@@ -153,28 +148,28 @@ export async function getPostsByUserHandler(
 
 export async function updatePostHandler(
   req: Request<UpdatePostInput["params"], {}, UpdatePostInput["body"]>,
-  res: Response
+  res: Response<{}, { user: UserWithAllFollows }>
 ) {
-  const user: FindUserResult = res.locals.user;
+  const user = res.locals.user;
   const postId = req.params.postId;
 
-  const post = await findPost({ _id: postId });
+  if (!user) {
+    return res.sendStatus(403);
+  }
+
+  const post = await findPost({ where: { id: postId } });
 
   if (!post) {
     return res.sendStatus(404);
   }
 
-  if (user.id !== post.author.id) {
+  if (user.id !== post.authorId) {
     return res.sendStatus(403);
   }
 
-  const update: UpdateQuery<Post> = {
-    ...req.body,
-    lastEditDate: new Date(Date.now()),
-  };
-
-  const updatedPost = await findAndUpdatePost({ _id: postId }, update, {
-    new: true,
+  const updatedPost = await findAndUpdatePost({
+    where: { id: postId },
+    data: { ...req.body },
   });
 
   return res.json(updatedPost);
@@ -182,30 +177,29 @@ export async function updatePostHandler(
 
 export async function likePostHandler(
   req: Request<LikePostInput["params"], {}, LikePostInput["body"]>,
-  res: Response
+  res: Response<{}, { user: UserWithAllFollows }>
 ) {
-  const like = JSON.parse(req.body.like);
-  const user: FindUserResult = res.locals.user;
+  const user = res.locals.user;
   const postId = req.params.postId;
+  const like: boolean = JSON.parse(req.body.like);
 
-  const post = await findPost({ _id: postId });
+  if (!user) {
+    return res.sendStatus(403);
+  }
+
+  const post = await findPost({ where: { id: postId } });
 
   if (!post) {
     return res.sendStatus(404);
   }
 
-  post.likes = post.likes.filter((userid) => userid != user.id);
+  const update = like
+    ? { connect: { id: user.id } }
+    : { disconnect: { id: user.id } };
 
-  if (like) {
-    post.likes.push(user.id);
-  }
-
-  const update = {
-    likes: post.likes,
-  };
-
-  const updatedPost = await findAndUpdatePost({ _id: postId }, update, {
-    new: true,
+  const updatedPost = await findAndUpdatePost({
+    where: { id: postId },
+    data: { likes: update },
   });
 
   return res.json(updatedPost);
@@ -213,27 +207,26 @@ export async function likePostHandler(
 
 export async function deletePostHandler(
   req: Request<DeletePostInput["params"]>,
-  res: Response
+  res: Response<{}, { user: UserWithAllFollows }>
 ) {
-  const user: FindUserResult = res.locals.user;
+  const user = res.locals.user;
   const postId = req.params.postId;
 
-  const post = await findPost({ _id: postId });
+  if (!user) {
+    return res.sendStatus(403);
+  }
+
+  const post = await findPost({ where: { id: postId } });
 
   if (!post) {
     return res.sendStatus(404);
   }
 
-  if (user.id !== post.author.id) {
+  if (user.id !== post.authorId) {
     return res.sendStatus(403);
   }
 
-  const commentResult = await deleteManyComments({ post: postId });
+  const deletedPost = await deletePost({ where: { id: postId } });
 
-  const postResult = await deletePost({ _id: postId });
-
-  return res.json({
-    posts_deleted: postResult.deletedCount,
-    comments_deleted: commentResult.deletedCount,
-  });
+  return res.json(deletedPost);
 }
