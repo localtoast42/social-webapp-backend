372b477024ab20182a0d95980b005741ca5abac5
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-08-07 16:55:07 -0400

    Set isGuest to false when creating random user

--- src/utils/populateDatabase.ts
+++ src/utils/populateDatabase.ts
@@ -22,6 +22,7 @@ export function createRandomUserInput() {
     state: faker.location.state(),
     country: "United States",
     imageUrl: faker.image.avatarGitHub(),
+    isGuest: false,
   };
 
   return input;
