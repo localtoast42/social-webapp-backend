71ad6d70bd4db53084a8c644bb2d36f327bf60be
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-09-10 17:46:30 -0400

    Add take, skip args to getPostByUser query

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -200,6 +200,8 @@ export async function getPostsByUserHandler(
       ],
     },
     orderBy: { createdAt: "desc" },
+    take: req.query.limit ? parseInt(req.query.limit as string) : undefined,
+    skip: req.query.skip ? parseInt(req.query.skip as string) : undefined,
   };
 
   const posts = await findManyPostsWithAuthorAndLikes(query);
